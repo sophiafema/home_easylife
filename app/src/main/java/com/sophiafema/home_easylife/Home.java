480529b286ac63f2c2287a8ce381a6aaeb092b32
dietrict <theresa.dietrich@gmx.net>
2019-12-03 17:23:38 +0100

    Login mit Name: aund passwort: 1 angelegt, Home Screen angelegt

---
+++ app/src/main/java/com/sophiafema/home_easylife/Home.java
@@ -0,0 +1,39 @@
+package com.sophiafema.home_easylife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Home extends AppCompatActivity {
+
+    TextView tVHomeLiving;
+    TextView tVHomeSleeping;
+    TextView tVHomeHallway;
+    TextView tVHomeKitchen;
+    TextView tVHomeBath;
+
+
+    ImageView iVHomeLiving;
+    ImageView iVHomeSleeping;
+    ImageView iVHomeHallway;
+    ImageView iVHomeKitchen;
+    ImageView iVHomeBath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        tVHomeLiving = (TextView) findViewById(R.id.tVHomeLiving);
+        tVHomeSleeping = (TextView) findViewById(R.id.tVHomeSleeping);
+        tVHomeHallway = (TextView) findViewById(R.id.tVHomeHallway);
+        tVHomeKitchen = (TextView) findViewById(R.id.tVHomeKitchen);
+        tVHomeBath = (TextView) findViewById(R.id.tVHomeBath);
+
+
+
+
+    }
+}
--- app/src/main/java/com/sophiafema/home_easylife/MainActivity.java
+++ app/src/main/java/com/sophiafema/home_easylife/MainActivity.java
@@ -2,14 +2,65 @@ package com.sophiafema.home_easylife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    private EditText eTMainName;
+    private EditText eTMainPasswort;
+    private Button btnMainLogIn;
+    private TextView tVMainInfo;
+    private int counter = 5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //setContentView(R.layout.activity_main);
+
+
+        eTMainName = (EditText) findViewById(R.id.eTMainName);
+        eTMainPasswort = (EditText) findViewById(R.id.eTMainPasswort);
+        tVMainInfo = (TextView) findViewById(R.id.tVMainInfo);
+        btnMainLogIn = (Button) findViewById(R.id.btnMainLogIn);
+
+
+        tVMainInfo.setText("Anzahl fehlerhafter Anmeldeversuche: 5");
+
+
+        btnMainLogIn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                validate(eTMainName.getText().toString(), eTMainPasswort.getText().toString());
+            }
+        });
+
+    }
+
+    private void validate(String nutzerName, String nutzerPasswort)
+    {
+        if((nutzerName.equals("a")) && (nutzerPasswort.equals("1")))
+        {
+            Intent intent = new Intent(MainActivity.this, Home.class);
+            startActivity(intent);
+        }
+        else
+        {
+            counter--;
+
+            tVMainInfo.setText("Anzahl fehlerhafter Anmeldeversuche: " + String.valueOf(counter));
+
+            if(counter == 0)
+            {
+                btnMainLogIn.setEnabled(false);
+            }
+        }
     }
 }
