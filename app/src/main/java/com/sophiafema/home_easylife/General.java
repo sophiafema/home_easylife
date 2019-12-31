42721e5b46fe11861258711041b04b8e46e7afd6
dietrict <theresa.dietrich@gmx.net>
2019-12-31 13:10:47 +0100

    General: Status Text (Licht, Temperatur, Musik)

--- app/src/main/java/com/sophiafema/home_easylife/General.java
+++ app/src/main/java/com/sophiafema/home_easylife/General.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -51,6 +52,7 @@ public class General extends AppCompatActivity implements View.OnClickListener{
         swGeneralTemperature = (Switch) findViewById(R.id.swGeneralTemperature);
         swGeneralMusic = (Switch) findViewById(R.id.swGeneralMusic);
 
+
         iVGeneralShuttersUp = (ImageView) findViewById(R.id.iVGeneralShuttersUp);
         iVGeneralShuttersDown = (ImageView) findViewById(R.id.iVGeneralShuttersDown);
 
@@ -58,6 +60,46 @@ public class General extends AppCompatActivity implements View.OnClickListener{
         tVGeneralTemperature = (TextView) findViewById(R.id.tVGeneralTemperature);
         tVGeneralMusic = (TextView) findViewById(R.id.tVGeneralMusic);
         tVGeneralShutters = (TextView) findViewById(R.id.tVGeneralShutters);
+
+
+        //Status Text Licht
+        swGeneralLight.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked == true) {
+                    tVGeneralLight.setText("Licht an");
+                }else{
+                    tVGeneralLight.setText("Licht aus");
+                }
+
+            }
+        });
+
+        //Status Text Temperatur
+        swGeneralTemperature.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked == true) {
+                    tVGeneralTemperature.setText("Thermostat an");
+                }else{
+                    tVGeneralTemperature.setText("Thermostat aus");
+                }
+
+            }
+        });
+
+        //Status Text Musik
+        swGeneralMusic.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked == true) {
+                    tVGeneralMusic.setText("Musik an");
+                }else{
+                    tVGeneralMusic.setText("Musik aus");
+                }
+
+            }
+        });
     }
 
     @Override
