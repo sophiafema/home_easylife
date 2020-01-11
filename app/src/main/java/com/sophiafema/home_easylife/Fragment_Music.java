f46ebcafaad25e6e8e4e6add79dd213ca81287de
anjacrypto <fuchs31@hotmail.de>
2020-01-11 19:43:45 +0100

    Musiktitel

--- app/src/main/java/com/sophiafema/home_easylife/Fragment_Music.java
+++ app/src/main/java/com/sophiafema/home_easylife/Fragment_Music.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.sophiafema.home_easylife.database.DatabaseAdapter;
 import com.sophiafema.home_easylife.models.Room;
@@ -22,11 +23,13 @@ public class Fragment_Music extends Fragment implements View.OnClickListener
     boolean play;
     boolean power;
     DatabaseAdapter db;
+    int musiccounter = 0;
 
-    com.sophiafema.home_easylife.view.TemperaturePicker pFMusik;
+    com.sophiafema.home_easylife.view.VolumePicker pFMusik;
     ImageView iVFMusicFF;
     ImageView iVFMusicBack;
     ImageView iVFMusicPlayPause;
+    TextView tVFMusicTitel;
 
     public Fragment_Music(Room room) {
         // Required empty public constructor
@@ -45,30 +48,43 @@ public class Fragment_Music extends Fragment implements View.OnClickListener
         play = r.getMusic().isPlay();
         power = r.getMusic().isPower();
 
-        String musiclist [] = {"Mamma Mia - ABBA", "Better - Nico Santos", "Let It Be - Beatles", "Sixteen - Ellie Goulding", "SOS - Avicii" };
-        int musiccounter = 0;
+        final String musiclist [] = {"Mamma Mia - ABBA", "Better - Nico Santos", "Let It Be - Beatles", "Sixteen - Ellie Goulding", "SOS - Avicii" };
 
+        tVFMusicTitel = (TextView) music.findViewById(R.id.tVFMusicTitel);
         iVFMusicPlayPause = (ImageView) music.findViewById(R.id.iVFMusicPlayPause);
-        if (play == true)
+        if (play)
         {
             iVFMusicPlayPause.setImageResource(android.R.drawable.ic_media_pause);
             play = false;
         }
-        else if (play == false)
+        else
         {
             iVFMusicPlayPause.setImageResource(android.R.drawable.ic_media_play);
             play = true;
         }
 
         //TODO WIe sonst?
-        //pFMusik = (com.sophiafema.home_easylife.view.VolumePicker) music.findViewById(R.id.pFMusic);
-        //pFMusik.setOnClickListener(this);
+        pFMusik = (com.sophiafema.home_easylife.view.VolumePicker) music.findViewById(R.id.pFMusic);
+        pFMusik.setOnClickListener(this);
 
         iVFMusicFF = (ImageView) music.findViewById(R.id.iVFMusicFF);
         iVFMusicFF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                musiccounter = musiccounter+1;
+                if (musiccounter <= 4) {
+                    switch (musiccounter) {
+                        case 1: tVFMusicTitel.setText(musiclist[0]);break;
+                        case 2: tVFMusicTitel.setText(musiclist[1]);break;
+                        case 3: tVFMusicTitel.setText(musiclist[2]);break;
+                        case 4: tVFMusicTitel.setText(musiclist[3]);break;
+                    }
+                }
+                else if (musiccounter ==5)
+                {
+                    musiccounter = 0;
+                    tVFMusicTitel.setText(musiclist[4]);
+                }
             }
         });
 
