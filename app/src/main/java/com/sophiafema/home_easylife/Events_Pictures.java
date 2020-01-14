6cbe097887949d18e37ecb1a719815167144df2a
dietrict <theresa.dietrich@gmx.net>
2020-01-14 15:46:00 +0100

    BottomSheet in Events Bilder importiert

---
+++ app/src/main/java/com/sophiafema/home_easylife/Events_Pictures.java
@@ -0,0 +1,14 @@
+package com.sophiafema.home_easylife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Events_Pictures extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events__pictures);
+    }
+}
